9bed45fbc7f518f223b776868309c21d3c8d213a
Djalal Harouni <tixxdz@gmail.com>
2022-08-17 00:06:01 +0200

    bpf:cgroup: set the tracking Cgroup ID of tasks during fork
    When a task forks we will query the cgroup tracking level, if it has
    a deeper nested level then we use the Tetragon cgroup level and assign it to
    the task execve_map_value in the execve_map. This way all forked childs
    will belong to the same tracked cgroup.
    
    If it has a lower cgroup tracking level, this is probably a service, pod,
    container being created and in this case we assign the current task cgroup
    id as the tracking cgroup id.
    
    This will allow later Tetragon to track all processes that have started
    inside a container/pod easily, where they will be assigned a cgroup tracking
    ID that should not change during their life cycle even if the task's cgroup
    config changes, migrates within pod, etc. the tracking is pinned for the
    task life time.
    
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

--- bpf/process/bpf_fork.c
+++ bpf/process/bpf_fork.c
@@ -6,7 +6,9 @@
 #include "bpf_tracing.h"
 
 #include "hubble_msg.h"
+#include "bpf_cgroup.h"
 #include "bpf_events.h"
+#include "environ_conf.h"
 #include "bpf_process_event.h"
 
 char _license[] __attribute__((section("license"), used)) = "GPL";
@@ -16,25 +18,46 @@ int _version __attribute__((section(("version")), used)) =
 #endif
 
 __attribute__((section("kprobe/wake_up_new_task"), used)) int
-BPF_KPROBE(event_wake_up_new_task, struct task_struct *task)
+BPF_KPROBE(event_wake_up_new_task, struct task_struct *new_task)
 {
 	struct execve_map_value *curr, *parent;
-	u32 pid = 0;
+	struct task_struct *current_task;
+	struct tetragon_conf *config;
+	u32 pid = 0, ppid = 0;
+	int zero = 0;
 
-	if (!task)
+	if (!new_task)
 		return 0;
 
-	probe_read(&pid, sizeof(pid), _(&task->tgid));
+	probe_read(&pid, sizeof(pid), _(&new_task->tgid));
 	curr = execve_map_get(pid);
 	if (!curr)
 		return 0;
+
+	/* Cgroup Tracking level */
+	config = map_lookup_elem(&tg_conf_map, &zero);
+	if (config && config->tg_cgrp_level > 0) {
+		/* Set the tracking cgroup ID for the new task if not already set */
+		__set_task_cgrpid_tracker(config, new_task, curr);
+
+		/* Let's try to catch current or "parent" if it was not tracked */
+		current_task = (struct task_struct *)get_current_task();
+		probe_read(&ppid, sizeof(ppid), _(&current_task->tgid));
+		/* If they share same thread group nothing todo... */
+		if (pid != ppid) {
+			parent = execve_map_get(ppid);
+			if (parent)
+				__set_task_cgrpid_tracker(config, current_task,
+							  parent);
+		}
+	}
 
 	/* generate an EVENT_COMMON_FLAG_CLONE event only once per process */
 	if (curr->key.ktime != 0)
 		return 0;
 
 	curr->flags = EVENT_COMMON_FLAG_CLONE;
-	parent = __event_find_parent(task);
+	parent = __event_find_parent(new_task);
 	if (parent) {
 		curr->key.pid = pid;
 		curr->key.ktime = ktime_get_ns();
